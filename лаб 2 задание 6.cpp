7bc3b696c6b7f90b4aee03b2a0257928aff6d101
Гриша Васильев <gregory.vasiliev@gmail.com>
2024-03-10 12:35:32 +0300

    Добавьте файлы проекта.

---
+++ лаб 2 задание 6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	ifstream file("f.txt");
+	double summ = 0;
+	long double multip = 1;
+	double num;
+	if (!file.is_open()) {
+		cout << "Не удалось открыть файл" << endl;
+	}
+	while (file >> num) {
+		summ += num;
+		multip *= num;
+	}
+	cout << "Сумма: " << abs(summ) << endl;
+	cout << "Произведение: " << multip*multip << endl;
+	file.close();
+}
